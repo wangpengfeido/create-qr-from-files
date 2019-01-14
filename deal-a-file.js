5abf72fbb08e8d3e494c1297eeb878e1fe72c211
Wangpengfei <1036793130@qq.com>
2019-01-14 16:12:53 +0800

    ...

--- deal-a-file.js
+++ deal-a-file.js
@@ -1,10 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-
 const qr = require('qr-image');
 
 const TEST_REGEX = new RegExp(require('./config').TEST_REGEX, 'g');
-const {QR_CODES_DIR} = require('./config');
 
 function search(str, regex) {
   const result = [];
@@ -21,13 +17,16 @@ function search(str, regex) {
 /**
  * 处理文件
  * @param {String} data
+ * @return {Array<ReadableStream>} 返回一个可读流数组
  */
-function dealAFile(data, toWriteFileName) {
+function dealAFile(data) {
   const searchReslut = [...new Set(search(data, TEST_REGEX))];
+  const result = [];
   searchReslut.forEach((item, index) => {
     const qr_png = qr.image(item, {type: 'png'});
-    qr_png.pipe(fs.createWriteStream(path.resolve(QR_CODES_DIR, `${toWriteFileName.split('.')[0]}_${index}.png`)));
+    result.push(qr_png);
   });
+  return result;
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const path = require('path');
 const fs = require('fs');
 
 const {FILES_TO_READ_DIR} = require('./config');
+const {QR_CODES_DIR} = require('./config');
 
 const {dealAFile} = require('./deal-a-file');
 
 fs.readdir(FILES_TO_READ_DIR, ((err, files) => {
-  files.forEach((item)=>{
+  let resultNumber = 0;
+  files.forEach((item) => {
     fs.readFile(path.resolve(FILES_TO_READ_DIR, item), {encoding: 'utf8'}, ((err, data) => {
-      dealAFile(data, item);
+      const dealResult = dealAFile(data, item);
+      dealResult.forEach(resultItem => {
+        resultNumber++;
+        resultItem.pipe(fs.createWriteStream(path.resolve(QR_CODES_DIR, `${resultNumber}.png`)));
+      });
     }));
   });
 
